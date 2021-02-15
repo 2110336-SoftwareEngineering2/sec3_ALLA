ac125c20680c8c6cd2ab4f1fc7b0a61a83821214
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2021-02-15 12:47:56 +0700

    add path

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,11 +1,18 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import MyProfile from '../pages/MyProfile';
 
-const  Routes=()=> {
+const Routes = () => {
     const location = useLocation();
     return (
         <div>
-            ROUTER
+            <Switch>
+            <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/profile" component={MyProfile} />
+            </Switch>
         </div>
     )
 }
