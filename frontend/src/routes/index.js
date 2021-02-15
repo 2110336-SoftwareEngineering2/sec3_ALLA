bd65e7b101f10e0c940cb8b68db7b845f12e8124
Chayut Prawattiyakul <c.prawattiyakul@gmail.com>
2021-02-15 16:03:18 +0700

    ตกแต่งหน้า login

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,7 @@ import { Route, Switch, useLocation } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import MyProfile from '../pages/MyProfile';
+import Register from '../pages/Register';
 
 const Routes = () => {
     const location = useLocation();
@@ -11,6 +12,7 @@ const Routes = () => {
             <Switch>
             <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
                 <Route exact path="/profile" component={MyProfile} />
             </Switch>
         </div>
