f80f2d8950bffefd1899abc684e45c8cf45a1da3
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2021-02-15 11:36:45 +0700

    create frontend branch

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,13 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+
+const  Routes=()=> {
+    const location = useLocation();
+    return (
+        <div>
+            ROUTER
+        </div>
+    )
+}
+export default Routes;
+
