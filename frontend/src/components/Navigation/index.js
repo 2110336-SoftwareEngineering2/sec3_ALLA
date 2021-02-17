c30beb4bdb78c20ac4c24df7055e387a73a8c238
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2021-02-17 21:41:08 +0700

    minor style changes

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,10 +5,10 @@ export default function Navigation() {
   //return <div><div className="nav-container">NAV</div><div className="nav-spacer"></div></div>;
   return <nav class="navbar navbar-dark bg-dark justify-content-space-between position-sticky">
     <div className="d-flex justify-content-between align-items-center nav-container ">
-      <div className="d-flex pr-15">
+      <div className="d-flex pr-10">
         <a className="navbar-brand ">NISITER</a>
       </div>
-      <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center pr-5">
         <input class="form-control" type="search" placeholder="Search" aria-label="Search"></input>
         <button class="btn btn-outline-success" type="submit">Search</button>
       </div>
