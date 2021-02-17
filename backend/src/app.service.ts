9a62e643c32aba4df233941b13b23a701efdda7b
rujaroon <ruj.aroon@gmail.com>
2021-02-17 18:06:43 +0700

    add student db initialization

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { getConnection } from 'typeorm';
 import { User, UserType } from 'src/entities/user.entity';
 import { UserService } from './user/user.service';
 import { compare, hash } from 'bcryptjs';
-
+import { Student } from './entities/student.entity';
 
 @Injectable()
 export class AppService {
@@ -14,15 +14,15 @@ export class AppService {
   }
 
   async initializeDB() {
-    const pw = await hash('test2',10);
+    const pw = await hash('xxx', 10);
     await getConnection()
       .createQueryBuilder()
       .insert()
       .into(User)
       .values([
         {
-          username: 'test2',
-          password:  pw,
+          username: 'xxx',
+          password: pw,
           type: UserType.EMPLOYER,
           email: 'test@example.com',
           firstName: 'fname',
@@ -31,8 +31,25 @@ export class AppService {
         },
       ])
       .execute();
-
-      
     console.log('user added');
+    const user = await this.userService.findByUsername('xxx');
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Student)
+      .values([
+          {
+            sid : Number(user.id),
+            birthDate: "11/11/1999",
+            university : "Chula",
+            degree: "Bachelor",
+            faculty : "Eng",
+            department : "Com",
+            fields_of_work : "something"
+        },
+      ])
+      .execute(); 
+
+    console.log('student added');
   }
 }
