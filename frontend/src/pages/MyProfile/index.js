50115ca79385d4e6a0a2eb56507955d6c485d541
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2021-02-23 13:29:37 +0700

    load myprofile data dynamically

--- frontend/src/pages/MyProfile/index.js
+++ frontend/src/pages/MyProfile/index.js
@@ -3,50 +3,79 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 
-async function onLoadHandler(id, token) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`http://192.168.1.120:8300/user/${id}`, {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      })
-      .then((response) => {
-        resolve(response.data);
-      })
-      .catch((error) => {
-        reject(error.message);
-      });
-  });
-}
 
 export default function MyProfile() {
+
 
   const userState = useSelector(state => state.Auth);
   const uid = userState.id;
   const token = userState.token;
 
- /*  useEffect(async () => {
-    const data = await onLoadHandler(uid, token);
-    console.log("data is: ", data);
-
-    //set state something
-  }); */
-
   const initProfile = {
-    fname: "Supawit",
-    lname: "Sutthiboriban",
+    firstName: "supawit",
+    lastName: "Sutthiboriban",
     department: "Computer Engineering",
     faculty: "Engineering",
     university: "Chulalongkorn University",
     email: "ssutthiboriban@yahoo.com",
-    phone: "+66862778800",
+    phoneNumber: "+66862778800",
     resume: "portfolio_url",
     img: "https://picsum.photos/200",
-    isStudent: true
+    fields_of_work: "",
+    degree: "",
+    id: 1,
+    sid: 1,
+    birthDate: "2021-02-20T18:59:20.255Z",
+    type: "STUDENT"
+
   };
 
+  //   
+  // firstName: ""
+  // lastName: ""
+  // department: ""
+  // faculty: ""
+  // university: ""
+  // email: ""
+  // phoneNumber: ""
+
+  // fields_of_work: ""
+  // degree: ""
+  // id: 1
+  // sid: 1
+  // birthDate: "2021-02-20T18:59:20.255Z"
+  // type: "STUDENT"
+
   const [profile, setProfile] = useState(initProfile);
+
+  async function onLoadHandler(id, token) {
+    await axios.get(`http://127.0.0.1:8300/user/${id}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => {
+        console.log('response', response)
+        setProfile({
+          ...response.data,
+          resume: initProfile.resume,
+          img: initProfile.img,
+          fields_of_work: initProfile.fields_of_work
+        })
+        return response
+      })
+      .catch(error => {
+        console.log(error)
+        return error
+      });
+  }
+
+  useEffect(() => {
+    onLoadHandler(uid, token);
+    //set state something
+  }, []);
+
+
   return (
     <div className="d-flex justify-content-around">
       <div class="form-row">
@@ -65,7 +94,7 @@ export default function MyProfile() {
         <form className="p-5">
           <div className="d-flex justify-content-center p-2 name_font-style">
             <text>
-              {profile.fname} {profile.lname}
+              {profile.firstName} {profile.lastName}
             </text>
           </div>
           <div className="d-flex justify-content-left p-2">
@@ -94,8 +123,8 @@ export default function MyProfile() {
           </div>
           <div className="d-flex justify-content-left p-2">
             <text>
-              <span className="font-weight-bold"> Phone Number : </span>{" "}
-              {profile.phone}
+              <span className="font-weight-bold"> Tel : </span>{" "}
+              {profile.phoneNumber}
             </text>
           </div>
           <div className="d-flex justify-content-left p-2">
@@ -109,7 +138,7 @@ export default function MyProfile() {
         {/* <form className="p-5">
           <div className="d-flex justify-content-center">
             <text>
-              {profile.fname} {profile.lname}
+              {profile.firstName} {profile.lastName}
             </text>
           </div>
           <div className="d-flex justify-content-center">
