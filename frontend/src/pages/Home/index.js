738dafeb74ef33d358ba43884db98cf4b5f16bc2
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2021-03-15 17:38:54 +0700

    filter style

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -8,19 +8,34 @@ import "./style.scss";
 
 export default function Home() {
     //เอา path มา ยิง api ตาม search and filters
+    //dispatch to set querry param in navigationbar
     const location = useLocation();
+    const getParamObj = () => {
+
+        const l = location.search.slice(1).split("&")
+        var retObj = {}
+        l.forEach((i, idx) => {
+            const a = i.split('=')
+            retObj[a[0]] = decodeURI(a[1])
+        })
+        return retObj
+    }
+
     const JobState = useSelector((state) => state.Job);
     console.log('location from home page', location)
     console.log('JOB LIST ', JobState.jobList)
     return (
-        <div>
-            <div className="d-flex justify-content-center ">
-                <SearchFilter></SearchFilter>
-            </div>
-            <div className="d-flex justify-content-center ">
-                
-                <JobPane jobList={JobState.jobList}></JobPane>
-
+        <div className="home-container d-flex justify-content-center">
+            <div className="d-flex flex-column col-sm-10 col-md-8 col-lg-8">
+                <div className="d-flex justify-content-center ">
+                    <SearchFilter param={getParamObj()} />
+                </div>
+                {JobState.jobList ? <div className="d-flex justify-content-center ">
+                    <JobPane jobList={JobState.jobList} />
+                </div> 
+                :
+                    <div>Sorry we cant find matching result</div>
+                }
             </div>
         </div>
     )
